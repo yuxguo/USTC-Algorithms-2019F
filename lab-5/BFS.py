e5286935a1b25d79bf76e4b156b979644e7294e8
YxGuo <gyx_20170818@mail.ustc.edu.cn>
2019-12-05 20:08:15 +0800

    add lab-5

---
+++ lab-5/BFS.py
@@ -0,0 +1,107 @@
+from graph import Graph
+import sys
+from queue import Queue
+
+
+
+def bfs_1(g, start_v, end_v):
+    flag = False
+    q = Queue()
+    v = start_v
+    q.put(v)
+    g.setVertexColor(v, g.gray)
+    while not q.empty():
+        v = q.get()
+        if v == end_v:
+            flag = True
+            break
+        l = g.getVertexAdjcent(v)
+        for u in l:
+            if g.getVertexColor(u) == g.white:
+                g.setVertexColor(u, g.gray)
+                g.setVertexDistance(u, g.getVertexDistance(v) + 1)
+                g.setVertexParent(u, v)
+                q.put(u)
+        g.setVertexColor(v, g.black)
+
+    if flag == True:
+        v = end_v
+        l = []
+        while v != start_v:
+            l.append(v)
+            v = g.getVertexParent(v)
+        print(v, end='')
+        for x in l[::-1]:
+            print('-> ', end='')
+            print(x, end='')
+    else: 
+        print("not find path")
+    return
+
+def bfs_2(g, start_v, end_v):
+    flag = False
+    q1 = Queue()
+    q2 = Queue()
+    v1 = start_v
+    v2 = end_v
+    q1.put(v1)
+    q2.put(v2)
+    g.setVertexColor(v1, g.gray)
+    g.setVertexColor(v2, g.gray)
+    while not q1.empty() and not q2.empty():
+        v1 = q1.get()
+        v2 = q2.get()
+        l1 = g.getVertexAdjcent(v1)
+        l2 = g.getVertexAdjcent(v2)
+        if v1 in l2:
+            flag =True
+            break
+        for u1 in l1:
+            if g.getVertexColor(u1) == g.white:
+                g.setVertexColor(u1, g.gray)
+                g.setVertexDistance(u1, g.getVertexDistance(v1) + 1)
+                g.setVertexParent(u1, v1)
+                q1.put(u1)
+        g.setVertexColor(v1, g.black)
+
+        for u2 in l2:
+            if g.getVertexColor(u2) == g.white:
+                g.setVertexColor(u2, g.gray)
+                g.setVertexDistance(u2, g.getVertexDistance(v2) + 1)
+                g.setVertexParent(u2, v2)
+                q2.put(u2)
+
+        g.setVertexColor(v2, g.black)
+    
+    if flag == True:
+        l = []
+        v = v1
+        while v != start_v:
+            l.append(v)
+            v = g.getVertexParent(v)
+        l.append(v)
+        l = l[::-1]
+        v = v2
+        while v != end_v:
+            l.append(v)
+            v = g.getVertexParent(v)
+        for x in l:
+            print(x, end='')
+            print('-> ', end='')
+        print(v)
+    else:
+        print("not find path")
+    return
+
+def main():
+    if len(sys.argv) != 5:
+        return
+    g = Graph(sys.argv[1])
+    if sys.argv[4] == '1':
+        bfs_1(g, int(sys.argv[2]), int(sys.argv[3]))
+    elif sys.argv[4] == '2':
+        bfs_2(g, int(sys.argv[2]), int(sys.argv[3]))
+    return 
+
+if __name__ == "__main__":
+    main()
---
+++ lab-5/graph.py
@@ -0,0 +1,83 @@
+from operator import itemgetter
+
+
+class Vertex(object):
+
+    def __init__(self, color, parent, distance):
+        self.color = color
+        self.parent = parent
+        self.distance = distance
+
+
+
+class Graph(object):
+
+    def __init__(self, filename):
+        with open(filename, "r") as f:
+            content = f.read()
+        content = content.split("\n")
+        pairs = list(map(lambda x:[int(e) for e in x.split(" ")], content))
+        pairs = pairs + [[x[1], x[0]] for x in pairs]
+        pairs = sorted(pairs, key = itemgetter(0,1))
+        self.__val = []
+        self.__col_ind = []
+        self.__row_ptr = []
+        prev_row = -1
+        for p in pairs:
+            if p[0] != prev_row:
+                self.__row_ptr.append(len(self.__val))
+                prev_row = p[0]
+            self.__val.append(1)
+            self.__col_ind.append(p[1])
+        
+        # add end
+        self.__row_ptr.append(len(self.__val))
+        self.black = -1
+        self.gray = 0
+        self.white = 1
+        self.nil = 0
+        self.inf = 10000000
+        self.__vertex = [Vertex(self.white, self.nil, self.inf) for x in range(len(self.__row_ptr) - 1)]
+        return 
+    
+
+    def getVertexAdjcent(self, v_index):
+        if v_index >= len(self.__vertex):
+            return False
+        s = self.__row_ptr[v_index]
+        e = self.__row_ptr[v_index+1]
+        v_list = [x for x in self.__col_ind[s:e]]
+        return v_list
+
+    def getVertexColor(self, v_index):
+        if v_index >= len(self.__vertex):
+            return False
+        return self.__vertex[v_index].color
+
+    def setVertexColor(self, v_index, color):
+        if color != self.white and color != self.white and color != self.gray:
+            return False
+        self.__vertex[v_index].color = color
+        return True
+    
+    def getVertexParent(self, v_index):
+        if v_index >= len(self.__vertex):
+            return False
+        return self.__vertex[v_index].parent
+
+    def setVertexParent(self, v_index, p_index):
+        if v_index >= len(self.__vertex) or p_index >= len(self.__vertex):
+            return False
+        self.__vertex[v_index].parent = p_index
+        return True
+
+    def getVertexDistance(self, v_index):
+        if v_index >= len(self.__vertex):
+            return False
+        return self.__vertex[v_index].distance
+
+    def setVertexDistance(self, v_index, distance):
+        if v_index >= len(self.__vertex):
+            return False
+        self.__vertex[v_index].distance = distance
+        return True
